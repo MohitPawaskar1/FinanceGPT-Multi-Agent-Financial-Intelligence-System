1b77679424bf5710c8cad032088e0a65ed89df00
MohitPawaskar1 <mohitpawaskar33@gmail.com>
2026-05-28 16:47:42 +0530

    Add sys and pathlib imports for path management

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -1,3 +1,12 @@
+import sys
+from pathlib import Path
+
+sys.path.append(
+    str(
+        Path(__file__).resolve().parent.parent
+    )
+)
+
 import streamlit as st
 import requests
 import pandas as pd
